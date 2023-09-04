e2b499f430f78256cd198b70d873fffe9e23311c
jeffnyak@gmail.com <jeffnyak@gmail.com>
2023-09-04 10:29:19 +0300

    Adding a node after a given node in linkedlist

--- linkedlist/insertion.py
+++ linkedlist/insertion.py
@@ -1,5 +1,5 @@
 """
-A new node in a linkedlist can be inserted the following position
+A new node in a linkedlist can be inserted in the following position:
 1. At the front of the linkedlist.
 2. After a given node
 3. At the end of the linkedlist
@@ -10,6 +10,17 @@ To insert a node at the front:
 1. Make the first node of the linkedlist to a new node
 2. Remove the original node from the head of the linkedlist
 3. Make the new node as the head of the linkedlist
+"""
+
+
+"""
+To insert a node after a given node:
+1. Check if the given node exist or not
+   -if the give node does not exist, then terminate the proces
+2. If the given node exist, then do the following
+   -Make the element to be inserted as the new node
+   -change next of the new node to next of the given node
+   -shift next of the given node to new node
 """
 
 class Node:
@@ -26,6 +37,15 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def addAfterNode(self, givenNode, data):
+        if not givenNode:
+            print("The given node does not exist")
+            return
+        new_node = Node(data)
+        new_node.next = givenNode.next
+        givenNode.next = new_node
+
+
 
     def printList(self):
         temp = self.head
@@ -37,4 +57,6 @@ ll = LinkedList()
 ll.addFront(10)
 ll.addFront(20)
 ll.addFront(30)
+
+ll.addAfterNode(ll.head.next, 120)
 ll.printList()
